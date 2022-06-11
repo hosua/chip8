4ceb77d68871adcbc49847eb48c2ab28f7875d7e
hosua <hosua.o@gmail.com>
2022-06-11 19:41:06 -0400

    proper opcodes are now being grabbed. turns out I was loading data into the ROM incorrectly

--- chip8-bare.cpp
+++ chip8-bare.cpp
@@ -123,6 +123,15 @@ public:
 	uint8_t gfx[DISP_X * DISP_Y] = {0}; // 64x32 display
 	uint8_t keys[NUM_KEYS] = {0};
 
+	uint8_t stack[STACK_SIZE] = {0}; // The 64-byte stack
+	uint8_t sp = 0x0; // 8-bit Stack pointer
+	uint8_t v[NUM_VREGS] = {0}; // Vx registers
+	uint16_t i = 0x0; // 16-bit index register. Stores memory addresses
+	uint16_t pc = 0x200; // Program counter (set it to the beginning of ROM)
+	uint8_t dt = 0x0; // Delay timer
+	uint8_t st = 0x0; // 8-bit Sound timer
+	uint16_t opcode = 0;
+	bool should_draw = false; // draw flag
 
 	bool LoadROM(const char* rom_path){
 		// Get length of file 
@@ -143,30 +152,33 @@ public:
 		// Most Chip-8 programs start at location 0x200 (512), but some begin at 0x600 (1536). 
 		// Programs beginning at 0x600 are intended for the ETI 660 computer.
 		for (int i = 0; i < rom_size; i++){
-			this->mem[0x200 + i] = (uint8_t)rom_buf[i];
+			this->mem[i + 0x200] = (uint8_t)rom_buf[i];
 		}
 		printf("Rom \"%s\" loaded into memory\n", rom_path);
 		free(rom_buf);
 		return true;
 	}
-};
 
-class CPU {
-public:
-	Chip8 chip8;
-	uint8_t stack[STACK_SIZE] = {0}; // The 64-byte stack
-	uint8_t sp = 0x0; // 8-bit Stack pointer
-	uint8_t v[NUM_VREGS] = {0}; // Vx registers
-	uint16_t i = 0x0; // 16-bit index register. Stores memory addresses
-	uint16_t pc = 0x200; // Program counter (set it to the beginning of ROM)
-	uint8_t dt = 0x0; // Delay timer
-	uint8_t st = 0x0; // 8-bit Sound timer
-	uint8_t *mem;
-	uint16_t opcode = 0;
-	bool should_draw = false; // draw flag
+					  
+	// Chip-8 instructions are 2 bytes (16-bits) long 
+	void cycle(){
+		// Fetch the next opcode (read 16 bits)
+		this->opcode = mem[this->pc] << 8 | mem[this->pc + 1];
+		uint8_t op = decode(this->opcode);
+		execute(op);
+		this->pc += 2; // increment program counter
+	}
 
-	CPU(Chip8 chip8){
-		this->mem = chip8.mem;
+	// Execute CPU instructions
+	void execute(uint8_t op){
+		const char* opstr = Op::optostr(op);
+		size_t x = Op::x(opcode); // x - A 4-bit value, the lower 4 bits of the high byte of the instruction
+		size_t y = Op::y(opcode); // y - A 4-bit value, the upper 4 bits of the low byte of the instruction
+		uint8_t kk = Op::kk(opcode); // kk or byte - An 8-bit value, the lowest 8 bits of the instruction
+		uint16_t nnn = Op::nnn(opcode); // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
+		uint8_t n = Op::n(opcode); // n or nibble - A 4-bit value, the lowest 4 bits of the instruction
+		printf("0x%04x: 0x%04x\n", this->pc, opcode);
+		
 	}
 
 	uint8_t decode(uint16_t opcode){
@@ -313,156 +325,6 @@ public:
 		}
 		return op;
 	}
-
-					  
-	// Chip-8 instructions are 2 bytes (16-bits) long 
-	void cycle(){
-		// Fetch the next opcode (read 16 bits)
-		this->opcode = mem[this->pc] << 8 | mem[this->pc + 1];
-		uint8_t op = decode(this->opcode);
-		execute(op);
-		this->pc += 2; // increment program counter
-	}
-
-	// Execute CPU instructions
-	void execute(uint8_t op){
-		const char* opstr = Op::optostr(op);
-		size_t x = Op::x(opcode); // x - A 4-bit value, the lower 4 bits of the high byte of the instruction
-		size_t y = Op::y(opcode); // y - A 4-bit value, the upper 4 bits of the low byte of the instruction
-		uint8_t kk = Op::kk(opcode); // kk or byte - An 8-bit value, the lowest 8 bits of the instruction
-		uint16_t nnn = Op::nnn(opcode); // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
-		uint8_t n = Op::n(opcode); // n or nibble - A 4-bit value, the lowest 4 bits of the instruction
-		printf("0x%04x: 0x%04x ", this->pc, opcode);
-
-		switch(op){
-		default:
-			printf("\nError: Invalid opcode {%04x}\n", opcode);
-			break;
-		case Op::SYS: // Ignored
-			printf("\n");
-			// printf("Ignoring SYS op\n");
-			break;
-		case Op::JP: 
-			switch(opcode & 0xF000){
-			default:
-				printf("\nError: Invalid opcode {%04x}\n", opcode);
-				break;
-			case 0x1000: // 1nnn - jump to address nnn
-				// this->pc = nnn;
-				this->i = nnn;
-				printf("JP 0x%x\n", nnn);
-				break;
-			case 0xB000: // Bnnn - jump to address nnn + v[0]
-				// this->i = nnn + this->v[0];
-				printf("JP 0x%03x + 0x%x\n", v[0], nnn);
-				break;
-			}
-			break;
-		case Op::LD: 	
-			switch(opcode & 0xF000){
-			default:
-				printf("\nError: Invalid opcode {%04x}\n", opcode);
-				break;
-			case 0x6000: // 6xkk - Set Vx to kk
-				this->v[x] = kk;
-				printf("%s V%i 0x%02x\n", opstr, x, kk);
-				break;
-			case 0x8000: // 8xy0 - Set Vx to Vy
-				this->v[x] = this->v[y];
-				printf("%s V%i V%i\n", opstr, this->v[x], this->v[y]);
-				break;
-			case 0xA000: // annn - Set i to address nnn
-				this->i = nnn;
-				printf("%s 0x%03x -> I\n", opstr, nnn);
-				// print_registers();
-				break;
-			case 0xF000:
-				switch(opcode & 0x00FF){
-				default:
-					printf("\nError: Invalid opcode {%04x}\n", opcode);
-					break;
-				case 0x0007: // Fx07 - LD Vx, DT "load Vx into DT"
-					this->dt = this->v[x];
-					printf("%s V%i DT\n", opstr, x);
-					break;
-				case 0x000A: // Fx0A - LD Vx, K
-					// this->v[x] = get_key();
-					printf("%s Vx, k NOT IMPLEMENTED\n", opstr);
-					break;
-				case 0x0015: // Fx15 - LD DT, Vx
-					this->v[x] = this->dt;
-					printf("%s DT V%i\n", opstr, x);
-					break;
-				case 0x0018: // Fx18 - LD ST, Vx
-					this->v[x] = this->st;
-					printf("%s ST V%i\n", opstr, x);
-					break;
-				case 0x0029: // Fx29 - LD F, Vx
-					// The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. 
-					this->i = this->v[x];
-					printf("%s F V%i\n", opstr, x);
-					break;
-				case 0x0033: // Fx33 - LD B, Vx
-					// TODO
-					// Store BCD representation of Vx in mem locations I, I+1, and I+2.
-					// BCD = Binary coded representation, see https://www.techtarget.com/whatis/definition/binary-coded-decimal
-					printf("%s NOT IMPLEMENTED\n", opstr);
-					break;
-				case 0x0055: // Fx55 - LD [I], Vx
-					// Store registers V0 through Vx in mem starting at location I (without modifications to I).
-					printf("%s NOT IMPLEMENTED\n", opstr);
-					break;
-				case 0x0065: // Fx65 - LD Vx, [I]
-					// Read from registers V0 through Vx in mem starting at location I.
-					printf("%s NOT IMPLEMENTED\n", opstr);
-					break;	
-				}
-			}
-		case Op::ADD: 	// Add kk to Vx
-			switch(opcode & 0xF000){
-				case 0x7000: // ADD Vx, byte
-					this->v[x] += kk;
-					printf("ADD V%i 0x%02x\n", kk);
-					break;
-				case 0x8000: // 8xy4 ADD Vx, Vy
-					this->v[y] += this->v[x]; 
-					printf("ADD V%i V%i\n", this->v[x], this->v[y]);
-					break;
-				case 0x001E: // Fx1E ADD I = I + Vx
-					this->i += this->v[x];
-					printf("ADD I, V%i\n", x);
-					break;
-			}
-			break;
-		case Op::SE:
-			printf("WARNING: %s NOT IMPLEMENTED\n", opstr);
-			break;
-		case Op::CALL:
-			this->sp++; // Increment stack pointer
-			this->pc = nnn; // Store address into program counter
-			this->stack[this->sp] = this->pc; // Put pc on top of the stack
-			printf("%s\n", opstr);
-			// print_registers();
-			// print_args(opcode, x, y, kk, nnn, n);
-			// printf("WARNING: %s NOT IMPLEMENTED\n", opstr);
-			break;
-		
-		case Op::RND: // RND Vx 
-			this->v[x] = (rand() % 0xFF) & 0xFF; // Set Vx to random # from (0-255), then & 255
-			printf("RND V%i = 0x%02x\n", x, v[x]);
-			break;
-		case Op::DRW:
-			printf("WARNING: %s NOT IMPLEMENTED\n", opstr);
-			break;
-		case Op::CLS: // Clear screen
-			printf("%s\n", opstr);
-			// Clear 64x32 display
-			for(int i = 0; i < DISP_X*DISP_Y; i++)
-				chip8.gfx[i] = 0;
-			should_draw = true;
-			break;
-		}
-	}
 	void print_registers(){
 		printf("------------\n");
 		printf("v registers:\n");
@@ -505,11 +367,10 @@ int main(int argc, char *argv[]){
 	printf("===============START================\n");
 	Chip8 chip8;
 	chip8.LoadROM(rom_path);
-	CPU cpu(chip8);
 	size_t cycles = 10;
 	for (int i = 0; i < cycles; i++){
 		// print_registers(cpu);
-		cpu.cycle();
+		chip8.cycle();
 	}
 	return 1;
 }
