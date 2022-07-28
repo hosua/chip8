f154f160fee03708207344e360cdf3f0122f3520
hosua <hosua.o@gmail.com>
2022-07-27 21:54:51 -0400

    change pong gif

---
+++ build/chip8.d
@@ -0,0 +1,2 @@
+build/chip8.o: src/chip8.cpp src/chip8.h src/display.h src/cpu.h \
+ src/clock.h
---
+++ build/input.d
@@ -0,0 +1,2 @@
+build/input.o: src/input.cpp src/input.h src/chip8.h src/display.h \
+ src/cpu.h src/clock.h
